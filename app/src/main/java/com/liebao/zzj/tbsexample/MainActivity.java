8058012fd7665b3b7e0458ea8747125165ac2ce3
sqoneal <zengzhongjiang@yeah.net>
2018-04-07 01:10:36 +0800

    更新地址栏随滚动消失

--- app/src/main/java/com/liebao/zzj/tbsexample/MainActivity.java
+++ app/src/main/java/com/liebao/zzj/tbsexample/MainActivity.java
@@ -1,15 +1,19 @@
 package com.liebao.zzj.tbsexample;
 
+import android.annotation.TargetApi;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.tencent.smtt.sdk.WebView;
 import com.tencent.smtt.sdk.WebViewClient;
@@ -23,17 +27,18 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     WebView mz_tbs_webview;
     Button mz_button;
     String mz_url;
+    LinearLayout mz_llayout1;
+    int mz_llayout1padding[];
 
-
-    Handler mz_handler = new Handler(){
+    Handler mz_handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            switch (msg.what){
+            switch (msg.what) {
                 case WEBVIEWGOBACK:
-                    if (mz_tbs_webview.canGoBack()){
+                    if (mz_tbs_webview.canGoBack()) {
                         mz_tbs_webview.goBack();
                         mz_edittext.setText(mz_tbs_webview.getUrl());
-                    }else{
+                    } else {
                         finish();
                     }
             }
@@ -52,17 +57,23 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         super.onDestroy();
     }
 
-    public void initview(){
+    @TargetApi(Build.VERSION_CODES.M)
+    public void initview() {
         mz_button = (Button) this.findViewById(R.id.mzButton);
         mz_edittext = (EditText) this.findViewById(R.id.mzEditText);
         mz_tbs_webview = (WebView) this.findViewById(R.id.mzTSBWebView);
-        mz_tbs_webview.getSettings().setUserAgentString(mz_tbs_webview.getSettings().getUserAgentString()+APP_NAME_UA);
-        //mz_tbs_webview.getSettings().setUserAgentString(APP_NAME_UA);
         mz_button.setOnClickListener(this);
+
+        mz_llayout1 = (LinearLayout) this.findViewById(R.id.llayout1);
+
+        mz_tbs_webview.getSettings().setUserAgentString(mz_tbs_webview.getSettings().getUserAgentString() + APP_NAME_UA);
+        //mz_tbs_webview.getSettings().setUserAgentString(APP_NAME_UA);
+        mz_llayout1padding = new int[]{mz_llayout1.getPaddingLeft(), mz_llayout1.getPaddingTop(),
+                mz_llayout1.getPaddingRight(), mz_llayout1.getPaddingBottom()};
         mz_url = mz_edittext.getText().toString();
         mz_tbs_webview.getSettings().setJavaScriptEnabled(true);
         mz_tbs_webview.loadUrl(mz_url);
-        mz_tbs_webview.setWebViewClient(new WebViewClient(){
+        mz_tbs_webview.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView webView, String s) {
                 return super.shouldOverrideUrlLoading(webView, s);
@@ -72,18 +83,45 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
             public void onPageStarted(WebView webView, String s, Bitmap bitmap) {
                 super.onPageStarted(webView, s, bitmap);
                 mz_edittext.setText(mz_tbs_webview.getUrl());
+                mz_llayout1.setPadding(mz_llayout1padding[0], mz_llayout1padding[1]
+                        , mz_llayout1padding[2], mz_llayout1padding[3]);
+            }
+        });
+        mz_tbs_webview.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                if (scrollY > oldScrollY) {
+                    mz_llayout1.setVisibility(View.INVISIBLE);
+                   /* mz_llayout1.setPadding(mz_llayout1.getPaddingLeft(), mz_llayout1.getPaddingTop() - scrollY + oldScrollY
+                            , mz_llayout1.getPaddingRight(), mz_llayout1.getPaddingBottom() - scrollY + oldScrollY);
+                    if (mz_llayout1.getPaddingTop() > -100) {
+                        mz_llayout1.setVisibility(View.INVISIBLE);
+                        mz_llayout1.setPadding(mz_llayout1padding[0], mz_llayout1padding[1]
+                                , mz_llayout1padding[2], mz_llayout1padding[3]);
+                    }*/
+
+                }
+                if (scrollY < oldScrollY) {
+                    mz_llayout1.setVisibility(View.VISIBLE);
+                    /*mz_llayout1.setPadding(mz_llayout1.getPaddingLeft(), mz_llayout1.getPaddingTop() + oldScrollY - scrollY
+                            , mz_llayout1.getPaddingRight(), mz_llayout1.getPaddingBottom() + oldScrollY - scrollY);
+                    if (mz_llayout1.getPaddingTop() < 100) {
+                        mz_llayout1.setPadding(mz_llayout1padding[0], mz_llayout1padding[1]
+                                , mz_llayout1padding[2], mz_llayout1padding[3]);
+                    }*/
+                }
             }
         });
     }
 
     @Override
     public void onClick(View v) {
-        if (v == mz_button){
+        if (v == mz_button) {
             mz_url = mz_edittext.getText().toString();
-            if (mz_url == null){
+            if (mz_url == null) {
                 mz_url = this.getResources().getString(R.string.index_site);
             }
-            if (!(mz_url.startsWith("http://")||mz_url.startsWith("https://"))){
+            if (!(mz_url.startsWith("http://") || mz_url.startsWith("https://"))) {
                 mz_url = "http://" + mz_url;
             }
             mz_tbs_webview.loadUrl(mz_url);
@@ -92,7 +130,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if(keyCode == KeyEvent.KEYCODE_BACK){
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
             mz_handler.sendEmptyMessage(WEBVIEWGOBACK);
             return false;
         }
