d9770254fdfd0ba6c2ba2501348580addb7f2e39
sqoneal <zengzhongjiang@yeah.net>
2018-05-15 20:43:19 +0800

    增加fragment Right的可从用，增加关于页面

--- app/src/main/java/com/liebao/zzj/tbsexample/MzFragmentLeft.java
+++ app/src/main/java/com/liebao/zzj/tbsexample/MzFragmentLeft.java
@@ -11,8 +11,13 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MzFragmentLeft extends Fragment implements View.OnClickListener {
+    public static final int TOBOOKMARK = 0X001;
+    public static final int TOABOUT = 0X003;
     private TextView mz_fg_left_bookmark_textview;
+    private TextView mz_fg_left_about_textview;
     private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private MzFragmentRight mzFragmentRight;
 
     public MzFragmentLeft(FragmentManager fragmentManager) {
         this.fragmentManager = fragmentManager;
@@ -24,7 +29,10 @@ public class MzFragmentLeft extends Fragment implements View.OnClickListener {
         View view = inflater.inflate(R.layout.fg_left, container, false);
         mz_fg_left_bookmark_textview = (TextView) view.findViewById(R.id.fg_bookmark_textview);
         mz_fg_left_bookmark_textview.setOnClickListener(this);
+        mz_fg_left_about_textview = (TextView) view.findViewById(R.id.fg_about_textview);
+        mz_fg_left_about_textview.setOnClickListener(this);
 
+        //mz_fg_left_about_textview.performClick();
         mz_fg_left_bookmark_textview.performClick();
         return view;
     }
@@ -32,15 +40,20 @@ public class MzFragmentLeft extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         if (v == mz_fg_left_bookmark_textview) {
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            MzFgBookmark mzFgBookmark = new MzFgBookmark();
-            Bundle bd = new Bundle();
-            bd.putString("page", "bookmarks");
-            mzFgBookmark.setArguments(bd);
-
-            fragmentTransaction.replace(R.id.llayout_setting_right, mzFgBookmark);
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit();
+            drumpRightPage(TOBOOKMARK);
+        }else if(v == mz_fg_left_about_textview){
+            drumpRightPage(TOABOUT);
         }
+    }
+
+    private void drumpRightPage(int key){
+        fragmentTransaction = fragmentManager.beginTransaction();
+        mzFragmentRight = new MzFragmentRight();
+        Bundle bd = new Bundle();
+        bd.putInt("page", key);
+        mzFragmentRight.setArguments(bd);
+        fragmentTransaction.replace(R.id.llayout_setting_right, mzFragmentRight);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
